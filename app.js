18128f1d0fc8eb1726ced45dddf888edfcd39501
Rizkiee <159923656+Rizkiee@users.noreply.github.com>
2026-03-20 01:09:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -396,7 +396,9 @@ const more = extra > 0
   : ""
 
     days.push(`
-      <div class="calendar-day ${isToday ? "active" : ""}" 
+      <div class="calendar-day 
+      ${isToday ? "active" : ""} 
+      ${selectedDate === dateStr ? "selected" : ""}"
       onclick="selectDate('${dateStr}')">
         <h4>${d.toLocaleDateString("id-ID",{weekday:"short"})}</h4>
         <span>${d.getDate()}</span>
